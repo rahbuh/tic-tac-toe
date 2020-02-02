1353049292c743fe20dcaac80c80e806705d9e5a
Robert Kramer <robert.n.kramer@gmail.com>
2020-02-02 00:15:29 -0800

    refactored game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 (function() {
-  const board = document.getElementById("board");
   const start = document.getElementById("start-btn");
-  const squares = document.getElementsByClassName("square");
   const firstPlayer = document.getElementById("select-player");
-  const radioX = document.getElementById("X");
-  const radioO = document.getElementById("O");
-  const message = document.getElementById("message");
+  const board = document.getElementById("board");
 
   let player = null;
   let gameOn = false;
-
   const scores = [
     [0, 0, 0],
     [0, 0, 0],
@@ -22,25 +17,55 @@
 
   start.addEventListener("click", () => {
     if (player) {
-      startGame();
       clearSquares();
       resetScores();
+      disablePlayerSelect();
       displayMessage(`${player}'s Move`);
+      startGame();
     }
   });
+
+  function clearSquares() {
+    const squares = document.getElementsByClassName("square");
+    for (const square of squares) {
+      square.innerText = "";
+    }
+  }
+
+  function resetScores() {
+    scores.forEach(score => {
+      score[0] = 0;
+      score[1] = 0;
+      score[2] = 0;
+    });
+  }
+
+  function disablePlayerSelect() {
+    document
+      .querySelectorAll('[type = "radio"]')
+      .forEach(radio => radio.setAttribute("disabled", true));
+  }
+
+  function displayMessage(msg) {
+    document.getElementById("message").innerText = msg;
+  }
+
+  function startGame() {
+    if (!gameOn) {
+      gameOn = true;
+    }
+  }
 
   board.addEventListener("click", e => {
     if (gameOn) {
       const isEmptySquare = e.target.innerText === "";
       const squareNum = e.target.dataset.square;
-      let winner;
 
       if (isEmptySquare) {
         e.target.innerText = player;
         updateScores(squareNum);
-        winner = evaluateScore();
 
-        if (winner) {
+        if (isPlayerWinner()) {
           displayMessage(`${player} Wins!`);
           endGame();
         } else if (!openSquares()) {
@@ -54,36 +79,11 @@
     }
   });
 
-  function startGame() {
-    if (!gameOn) {
-      gameOn = true;
-    }
-    radioO.setAttribute("disabled", true);
-    radioX.setAttribute("disabled", true);
-  }
-
-  function clearSquares() {
-    const squaresArr = [...squares];
-    squaresArr.forEach(square => (square.innerText = ""));
-  }
-
-  function resetScores() {
-    scores.forEach(score => {
-      score[0] = 0;
-      score[1] = 0;
-      score[2] = 0;
-    });
-  }
-
-   function displayMessage(msg) {
-    message.innerText = msg;
-  }
-
-  function updateScores(choice) {
+  function updateScores(square) {
     let value;
     player === "X" ? (value = 1) : (value = -1);
 
-    switch (choice) {
+    switch (square) {
       case "1":
         scores[0][0] = value;
         break;
@@ -116,31 +116,39 @@
     }
   }
 
-  function evaluateScore() {
+  function isPlayerWinner() {
     let win;
+    let isWinner = false;
     player === "X" ? (win = 3) : (win = -3);
 
-    for (let index = 0; index < 3; index++) {
-      if (scores[index][0] + scores[index][1] + scores[index][2] === win) {
-        return true;
-      }
-      if (scores[0][index] + scores[1][index] + scores[2][index] === win) {
-        return true;
-      }
-    }
+    const possibleWins = [
+      [...scores[0]],
+      [...scores[1]],
+      [...scores[2]],
+      [scores[0][0], scores[1][0], scores[2][0]],
+      [scores[0][1], scores[1][1], scores[2][1]],
+      [scores[0][2], scores[1][2], scores[2][2]],
+      [scores[0][0], scores[1][1], scores[2][2]],
+      [scores[0][2], scores[1][1], scores[2][0]]
+    ];
 
-    if (scores[0][0] + scores[1][1] + scores[2][2] === win) {
-      return true;
-    }
-    if (scores[0][2] + scores[1][1] + scores[2][0] === win) {
-      return true;
-    }
-    return false;
+    possibleWins.forEach(scoresArr => {
+      const result = scoresArr.reduce((total, num) => {
+        return total + num;
+      });
+      if (result === win) {
+        isWinner = true;
+      }
+    });
+
+    return isWinner;
   }
 
   function openSquares() {
-    for (let index = 0; index < squares.length; index++) {
-      if (squares[index].innerText === "") {
+    const squares = document.getElementsByClassName("square");
+
+    for (const square of squares) {
+      if (square.innerText === "") {
         return true;
       }
     }
@@ -150,9 +158,9 @@
   function endGame() {
     gameOn = false;
     player = null;
-    radioO["checked"] = false;
-    radioX["checked"] = false;
-    radioO.removeAttribute("disabled");
-    radioX.removeAttribute("disabled");
+    document.querySelectorAll('[type = "radio"]').forEach(radio => {
+      radio.removeAttribute("disabled");
+      radio["checked"] = false;
+    });
   }
 })();
